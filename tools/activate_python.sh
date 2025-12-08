8255e92594366a6b2c595978b974a97cbf7b24bb
nagohachi <nagohachi.study@gmail.com>
2025-12-09 01:15:16 +0900

    modified activate_python.sh

--- tools/activate_python.sh
+++ tools/activate_python.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Workaround for conda activate scripts expecting variables to be set
+# Temporarily disable unbound variable errors during activation
+set +u
 eval "$(pixi shell-hook -s bash)"
+set -u
