f135e97144c6d2545b98c26604fe9c16cc0985b8
yukihira-pot <r.magoshi.ma54@gmail.com>
2025-12-08 22:30:45 +0900

    added nix dev

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Python Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python312
+            pkgs.uv
+            pkgs.sox
+            pkgs.ffmpeg
+            pkgs.cudaPackages_12_8.cudatoolkit
+            pkgs.cudaPackages_12_8.cudnn
+          ];
+          shellHook = ''
+            export PS1="(nix) $PS1"
+            echo "uv version: $(uv --version)"
+            echo "python version: $(python --version)"
+            echo "CUDA version: $(nvcc --version | grep release)"
+          '';
+        };
+      });
+}
